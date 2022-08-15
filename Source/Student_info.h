edcbbbd7345456bc2c6f751561a6d6185be0df37
aog1108 <aog1102@naver.com>
2022-08-15 19:11:35 +0900

    Implementing Class Polymorphism

--- Source/Student_info.h
+++ Source/Student_info.h
@@ -5,23 +5,84 @@
 #include <vector>
 #include <iostream>
 
-class Student_info {
+class Core {
+	friend class Student_info;
 public:
-	Student_info() : midterm(0), final(0) {}
-	Student_info(std::istream& is) { read(is); }
+	Core() : midterm(0), final(0) { }
+	Core(std::istream& is) { read(is); }
 
-	std::istream& read(std::istream&);
-	double grade() const;
+	virtual ~Core() { }
+
 	std::string name() const { return n; }
-	bool valid() const { return !homework.empty(); }
+	virtual std::istream& read(std::istream&);
+	virtual double grade() const;
+
+protected:
+	virtual Core* clone() { return new Core(*this); }
+	std::istream& read_common(std::istream&);
+	double midterm, final;
+	std::vector<double> homework;
 
 private:
 	std::string n;
-	double midterm, final;
-	std::vector<double> homework;
 };
 
-bool compare(const Student_info&, const Student_info&);
+class Grad : public Core {
+public:
+	Grad() : thesis(0) { }
+	Grad(std::istream& is) { read(is); }
+	double grade() const;
+	std::istream& read(std::istream&);
+
+protected:
+	Grad* clone() { return new Grad(*this); }
+
+private:
+	double thesis;
+};
+
+class Student_info {
+public:
+	Student_info() : cp(0) { }
+	Student_info(std::istream& is) : cp(0) { read(is); }
+	
+	Student_info(const Student_info& s) : cp(0) { if (s.cp) cp = s.cp->clone(); }
+	
+	Student_info& operator=(const Student_info& s)
+	{
+		if (&s != this) {
+			delete cp;
+			if (s.cp)
+				cp = s.cp->clone();
+			else
+				cp = 0;
+		}
+		return *this;
+	}
+	
+	~Student_info() { delete cp; }
+
+	std::istream& read(std::istream&);
+
+	std::string name() const {
+		if (cp) return cp->name();
+		else throw std::runtime_error("uninitialized Student");
+	}
+	double grade() const {
+		if (cp) return cp->grade();
+		else throw std::runtime_error("uninitialized Student");
+	}
+	static bool compare(const Student_info& s1, const Student_info& s2) {
+		return s1.name() < s2.name();
+	}
+
+private:
+	Core* cp;
+};
+
+bool compare(const Core&, const Core&);
+bool compare_Core_ptrs(const Core*, const Core*);
+bool compare_grades(const Core&, const Core&);
 
 std::istream& read_hw(std::istream&, std::vector<double>&);
 
